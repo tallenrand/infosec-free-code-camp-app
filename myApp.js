ddce02ad5a573e8a0120766203b7496d779eeab2
Overseer <tallenkeoni@gmail.com>
2023-10-27 02:06:49 -0700

    parent helmet example

--- myApp.js
+++ myApp.js
@@ -2,24 +2,18 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({action: 'deny'}));
-app.use(helmet.xssFilter());
-app.use(helmet.noSniff());
-app.use(helmet.ieNoOpen());
-const timeInSeconds = 90*24*60*60;
-app.use(helmet.hsts({maxAge: timeInSeconds, force: true}));
-app.use(helmet.dnsPrefetchControl());
-app.use(helmet.noCache());
-app.use(
-  helmet.contentSecurityPolicy({
+app.use(helmet({
+  frameguard: {         // configure
+    action: 'deny'
+  },
+  contentSecurityPolicy: {    // enable and configure
     directives: {
-    defaultSrc: ["'self'"],
-    scriptSrc: ["'self'", "trusted-cdn.com"],
+      defaultSrc: ["'self'"],
+      styleSrc: ['style.com'],
     }
-  })
-);
-
+  },
+  dnsPrefetchControl: false     // disable
+}))
 
 
 
